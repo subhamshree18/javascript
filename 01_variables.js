23062718d451285501f24a64a9b3b53ce7e5435f
subhamshree18 <subhamshree18mohanty@gmail.com>
2025-06-04 14:43:00 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const id=12345
+let name="subham"
+var place="odisha"
+dist="puri"
+let country
+// we can declare variable without using 'let','var','const' but it is not preferable and dont use 'var' beacuse it is a global variable 
+console.log(id,name,place,dist,country)
+console.table([id,name,place,dist,country])//it gives us output in a table like structure
