41a8acec61014293ec6002ff49d0b62087954e31
subhamshree18 <subhamshree18mohanty@gmail.com>
2025-06-04 14:49:40 +0000

    conversionoperation

---
+++ 03_conversionoperation.js
@@ -0,0 +1,61 @@
+// let score=33
+//  console.log(score);
+//  console.log(typeof(score))
+
+// let stringNumber=String(score)//here score is converted to string datatype
+// console.log(stringNumber);
+// console.log(typeof(stringNumber))
+
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1 false=>0
+
+
+
+// let booleanvalue1=Boolean(score)// here score is converted to boolean datatype
+// console.log(booleanvalue);
+// console.log(typeof(booleanvalue))
+
+/* 1=>true
+0=>false */
+
+// let isloggedin=""
+// console.log(isloggedin);
+// console.log(typeof(isloggedin))
+
+// let booleanvalue2=Boolean(isloggedin)
+// console.log(booleanvalue);
+// console.log(typeof(booleanvalue))
+
+
+// ""=>false
+//"subham"=>true
+
+/* 
+Number,String,Boolean these are the classes which converts the variables to corresponding datatypes
+*/
+
+// operation
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// % modulus
+// ++ increment
+// -- decrement
+// ** power 
+
+// let str1="subham"
+// let str2=" shree"
+// let str3= str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + "2");
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);//if we add a '+' sign before it then it converted to an integer 
+console.log(true + "");
+console.log(+"");// we know that ""=> false but if we add a '+' sign before it then it converted to an integer 
