b75fd3e7547ea76cd60cef4b6617d499fad62cef
subhamshree18 <subhamshree18mohanty@gmail.com>
2025-06-04 14:48:42 +0000

    datatypes

---
+++ 02_datatyes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) we use nodejs not browser it is a valid syntax in browser but not in nodejs
+let score=33
+console.log(typeof score);
+console.log(typeof (score));
+// typeof is used to show type of variable 
+//SOME DATATYPES used in javascript
+
+//number=>2 the power 53(range)
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol(it is mainly used react)
+//object
+console.log(typeof undefined)
+console.log(typeof null)
+//type of undefined is undefined and type of null is object
+//use MDN and  tc39.es documentation
