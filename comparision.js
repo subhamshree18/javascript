68bbb8899607e89be440455ba6856348404af430
subhamshree18 <subhamshree18mohanty@gmail.com>
2025-06-04 14:50:36 +0000

    comparision

---
+++ comparision.js
@@ -0,0 +1,21 @@
+/* 
+greater than
+lessthan
+greater than equal to
+less tha equal to
+double eqaul to
+not equal to
+these all are the comaprision operatior
+*/
+console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+/*
+the reason that equaltiy check and comparision works differently.comparision convert null to a number and treating it as a 0 
+
+=== it checks value as well as datatype
+*/ 
+console.log("2"==="2");
+console.log("2"==="2");
